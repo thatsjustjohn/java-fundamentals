119a3ce6d92bbc713f7ab06bc462b0876cd9ce41
John Winters <jhnwin@amazon.com>
2019-05-28 11:28:02 -0700

    finished everything but the stretch

--- basics/Main.java
+++ basics/Main.java
@@ -1,3 +1,6 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Main {
   public static void main(String[] args) {
     int dogCount = 1;
@@ -9,14 +12,43 @@ public class Main {
     int turtleCount = 0;
     System.out.println("I own " + turtleCount + " " + pluralize("turtle", turtleCount) + ".");
 
+    flipNHeads(1);
 
+    clock();
   }
 
-  // 
-  public static String pluralize(String animal, int count) {
-    if(count == 0 || count != 1){
-      animal = animal + "s";
+  // Function takes in a string and count and returns the correct version of the string pluralized.
+  public static String pluralize(String item, int count) {
+      return count == 1 ? item : item + 's';
+  }
+
+  // Function to flip a specific number of heads in a row
+  public static void flipNHeads(int headsInARow){
+    int numberOfFlips = 0;
+    int numberOfHeads = 0;
+    while(numberOfHeads < headsInARow){
+      if(Math.random() < .5){
+        System.out.println("tails");
+        numberOfHeads = 0;
+      }else{
+        System.out.println("heads");
+        numberOfHeads++;
+      }
+      numberOfFlips++;
     }
-    return animal;
+    System.out.printf("It took %d %s to flip %d %s in a row.\n", numberOfFlips, pluralize("flip", numberOfFlips), numberOfHeads, pluralize("head", numberOfHeads));
+  }
+
+  // Function to only print out each second when a new second happens
+  public static void clock(){
+    LocalDateTime previousSecond = LocalDateTime.now();
+    while(true){
+      LocalDateTime current = LocalDateTime.now();
+      if(previousSecond.getSecond() != current.getSecond()){
+        String time = current.format(DateTimeFormatter.ofPattern("HH:mm:ss"));
+        System.out.println(time);
+        previousSecond = current;
+      }
+    }
   }
 }
